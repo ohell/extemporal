00c5af60d63b6dd0ef6b194c0d2fb1bb16ad3af6
Amit <amit@lipik.in>
2013-08-13 00:57:35 +0100

    Added ModularMultiplication as an implementation of EncryptionAlgorithm. Refactored code, improved documentation.

--- src/net/extemporal/Cryptologistics.java
+++ src/net/extemporal/Cryptologistics.java
@@ -23,8 +23,6 @@ import java.nio.CharBuffer;
 import java.util.Random;
 import java.util.zip.CRC32;
 
-import java.security.SecureRandom;
-
 /**
  * This class encrypts/decrypts the messages using a pseudo-random one time pad.
  * The cipher length is always the same as the message length (very short messages are padded, though). <br/>
@@ -48,7 +46,7 @@ import java.security.SecureRandom;
 public class Cryptologistics {
 	public Cryptologistics()
 	{
-		algorithm = new SRA();
+		algorithm = new EncryptionAlgorithm.ModularMultiplication();
 	}
 
 	static int calculateEncryptedByteLength(int msgByteLength, boolean isPlaintext)
@@ -93,18 +91,9 @@ public class Cryptologistics {
 						key = ByteBuffer.wrap(keyBuffer).asCharBuffer();
 
 		for (int i = key.position(); plaintext.hasRemaining(); ++i) {
-			char k = key.get(), m = plaintext.get();
-
-			int cc = algorithm.encryptChar(m, k),
-					kk = cc > 1 && cc <= MAX ? algorithm.invertKeyChar(k) : MAX + 1;
-			if (kk <= MAX) {
-				cipher.put((char)cc);
-				key.put(i, (char)kk); // key becomes decryption key
-			}
-			else {	// either key or cipher is not representable.
-				cipher.put(m);
-				key.put(i, (char)1);
-			}
+			final int kc = algorithm.encryptChar(plaintext.get(), key.get(), msgIsPlaintext);
+			cipher.put((char)kc);
+			key.put(i, (char)(kc >>> 16));
 		}
 
 		final CRC32 c32 = new CRC32();
@@ -123,18 +112,9 @@ public class Cryptologistics {
 				c = 1;
 
 			for (nC = key.position(); c < footer.length; ++c) {
-				char k = key.get(), m = footer[c];
-
-				int cc = algorithm.encryptChar(m, k),
-						kk = cc > 1 && cc <= MAX ? algorithm.invertKeyChar(k) : MAX + 1;
-				if (kk <= MAX) {
-					cipher.put((char)cc);
-					key.put(nC++, (char)kk); // key becomes decryption key
-				}
-				else {	// either key or cipher is not representable.
-					cipher.put(m);
-					key.put(nC++, (char)1);
-				}
+				final int kc = algorithm.encryptChar(footer[c], key.get(), true);
+				cipher.put((char)kc);
+				key.put(nC++, (char)(kc >>> 16));
 			}
 		}
 		else {
@@ -156,13 +136,13 @@ public class Cryptologistics {
 
 		int iC = cipher.length(), iK = key.length();
 		if (recoveringPlaintext) {	// read footer, verify after reading rest of message
-			char c = algorithm.decryptChar(cipher.get(--iC), key.get(--iK));
-			msgBytes = c + (algorithm.decryptChar(cipher.get(--iC), key.get(--iK)) << 16);
+			char c = algorithm.decryptChar(cipher.get(--iC), key.get(--iK), true);
+			msgBytes = c + (algorithm.decryptChar(cipher.get(--iC), key.get(--iK), true) << 16);
 
-			paddingBytes = algorithm.decryptChar(cipher.get(--iC), key.get(--iK));
+			paddingBytes = algorithm.decryptChar(cipher.get(--iC), key.get(--iK), true);
 
-			c = algorithm.decryptChar(cipher.get(--iC), key.get(--iK));
-			chkSum = c + (algorithm.decryptChar(cipher.get(--iC), key.get(--iK)) << 16);
+			c = algorithm.decryptChar(cipher.get(--iC), key.get(--iK), true);
+			chkSum = c + (algorithm.decryptChar(cipher.get(--iC), key.get(--iK), true) << 16);
 
 			ensure(iK >= (msgBytes + 1) / 2, "The key length does not match the message length specified!");
 		}
@@ -181,7 +161,7 @@ public class Cryptologistics {
 
 		CharBuffer decryptChars = decryptBuffer.asCharBuffer();
 		while (--iK >= 0) {
-			decryptChars.put(iK, algorithm.decryptChar(cipher.get(--iC), key.get(iK)));
+			decryptChars.put(iK, algorithm.decryptChar(cipher.get(--iC), key.get(iK), recoveringPlaintext));
 		}
 
 		if (recoveringPlaintext) {
@@ -216,220 +196,4 @@ public class Cryptologistics {
 		if (!condition)
 			throw new IllegalArgumentException(msg);
 	}
-
-	static abstract class EncryptionAlgorithm {
-		abstract void obtainOneTimePad(byte[] pad);
-		abstract int encryptChar(char msgChar, char keyChar);
-		abstract int invertKeyChar(char keyChar);
-		abstract char decryptChar(char cipherChar, char keyChar);
-
-		protected EncryptionAlgorithm(final byte[] randomSeed)
-		{
-			randomGen = randomSeed != null && randomSeed.length > 0 ? new SecureRandom(randomSeed) : new SecureRandom();
-		}
-		protected final SecureRandom randomGen;
-	}
-
-	/**
-	Simple encryption by xor'ing the key and message words.<b/>
-	O(1) per word
-	*/
-	static class XOR extends EncryptionAlgorithm {
-		XOR()
-		{
-			super(null);
-		}
-
-		@Override
-		public void obtainOneTimePad(byte[] pad)
-		{
-			randomGen.nextBytes(pad);
-		}
-
-		@Override
-		public int encryptChar(char msgChar, char keyChar)
-		{	// returns int because possibility of result overflowing
-			return (char)(msgChar ^ keyChar);
-		}
-
-		@Override
-		public int invertKeyChar(char keyChar)
-		{
-			return keyChar;
-		}
-
-		@Override
-		public char decryptChar(char cipherChar, char keyChar)
-		{	// returns char because no possibility of result overflowing
-			return (char)(cipherChar ^ keyChar);
-		}
-	}
-
-	/**
-	See http://people.csail.mit.edu/~rivest/ShamirRivestAdleman-MentalPoker.pdf for details.<b/>
-	Encryption involves log(16) = 4 multiplications per word, and computation of decryption key
-	also involves 4 multiplications.
-	*/
-	static class SRA extends EncryptionAlgorithm {
-		SRA()
-		{
-			super(null);
-		}
-
-		public static boolean testAlgorithm(char b, char e)
-		{
-			if ((e & 1) == 0)
-				++e;
-
-			int c = Arithmetic.powMod(b, e, MOD_char);
-
-			int e2 = Arithmetic.powMod(e, totient_totient - 1, totient);
-
-			int b2 = Arithmetic.powMod(c, e2, MOD_char);
-			return b2 == b;
-		}
-
-		@Override
-		public void obtainOneTimePad(byte[] pad)
-		{
-			assert pad.length % 2 == 0;
-
-			randomGen.nextBytes(pad);
-
-			CharBuffer cb = ByteBuffer.wrap(pad).asCharBuffer();
-			for (int c = 0, nC = cb.length(); c < nC; ++c) {
-				int k = cb.get(c);
-				if ((k & 1) == 0)	// totient(n) is even for n >= 3 (Wolfram Alpha says)
-					++k;
-				while (nC < 0 && Arithmetic.gcd(totient, k) > 1) {
-					/*
-					Our totient is 2^n. Hence, it only has factors of the form 2^k.
-					Therefore, GCD check is unnecessary - tweaking each key char to be odd is
-					good enough.
-					 */
-					k += 2;
-					if (k == totient - 1)
-						break;
-				}
-				cb.put(c, (char)k);
-			}
-		}
-
-		@Override
-		public int encryptChar(char msgChar, char keyChar)
-		{
-			return Arithmetic.powMod(msgChar, keyChar, MOD_char);
-		}
-
-		@Override
-		public int invertKeyChar(char keyChar)
-		{
-			return Arithmetic.powMod(keyChar, totient_totient - 1, totient);
-		}
-
-		@Override
-		public char decryptChar(char cipherChar, char keyChar)
-		{
-			return (char)Arithmetic.powMod(cipherChar, keyChar, MOD_char);
-		}
-
-		/*
-		* Be warned - these constants are not extensible to integers, since Fermat primes stop at 2^4.
-		* Good thing about this scheme is that GCD check is not required, i.e GCD(key, totient) is
-		* a given for all odd keys.
-		 */
-		protected static final int MOD_char = (1 << 16), // Fermat prime
-									totient = MOD_char / 2,
-									totient_totient = totient / 2;	// totient(p^x) = p^x(1 - 1/p) src: Wolfram Alpha
-
-		static class Arithmetic {
-			static int powMod(int b, int e, int m)
-			{   // b^e | m
-				// IMPORTANT: Java long is signed, so max value 2^63 - 1. Big enough to hold max int (2^31 - 1)^2
-				// Note that temptation to create a specialization for chars, with base as int, is pointless since
-				// int is signed in Java, and 2^31 - 1 is not big enough to hold (2^16 - 1)^2
-				assert b >= 0 && e >= 0 && m > b;
-
-				if (b <= 1)
-					return b;
-
-				int result = 1;
-				if (e != 0) {
-					for (long base = b; true; e >>>= 1) {
-						if ((e & 1) == 1) {
-							result = (int)((base * result) % m);
-							if (e == 1)
-								break;
-						}
-						base = (base * base) % m;
-						//if (base == 0)	// check is sound, but maybe a rarely succeeding long comparison is overkill
-						//	return 0;
-					}
-				}
-				return result;
-			}
-
-			static boolean isPrime(int n)
-			{
-				if ((n & 1) == 0)   // even number
-					return false;
-
-				float xPrev = n, x = 0.5f * (n + 1);
-				while (Math.abs(xPrev - n) >= 1.0f) {   // calculate floor(sqrt(x))
-					xPrev = x;
-					x = 0.5f * (x + x/n);
-				}
-
-				for (int i = 3, nS = (int)x; i <= nS; i += 2) { // nS is floor(sqrt(n))
-					if (n % i == 0)
-						return false;
-				}
-				return true;
-			}
-
-			static int gcd(int a, int b)
-			{
-				while (a != 0) {
-					int x = a;
-					a = b % x;
-					b = x;
-				}
-				return b;
-			}
-
-			static int gcd_binary(int a, int b)
-			{
-				if (a == 0)
-					return b;
-				else if (b == 0)
-					return a;
-
-				// Make "a" and "b" odd, keeping track of common power of 2.
-				final int aTwos = Integer.numberOfTrailingZeros(a);
-				a >>= aTwos;
-				final int bTwos = Integer.numberOfTrailingZeros(b);
-				b >>= bTwos;
-				final int shift = Math.min(aTwos, bTwos);
-
-				// "a" and "b" are positive.
-				// If a > b then "gdc(a, b)" is equal to "gcd(a - b, b)".
-				// If a < b then "gcd(a, b)" is equal to "gcd(b - a, a)".
-				// Hence, in the successive iterations:
-				//  "a" becomes the absolute difference of the current values,
-				//  "b" becomes the minimum of the current values.
-				while (a != b) {
-					if (a < b) {
-						a = b - a;
-						b -= a;
-					}
-					else
-						a -= b;
-
-					a >>= Integer.numberOfTrailingZeros(a);	// Remove any power of 2 in "a" ("b" is guaranteed to be odd).
-				}
-				return a << shift;	// Recover the common power of 2.
-			}
-		}
-	}
-
 }
